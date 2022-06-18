18e753b055a8c6c8d398f793322050329324cb9b
Condesso1998 <ruicondesso98@gmail.com>
2022-06-18 22:20:16 +0100

    Criar o ContentProvider

---
+++ app/src/main/java/pt/ipg/trabalhofinal/ContentProviderTrabalhoFinal.kt
@@ -0,0 +1,45 @@
+package pt.ipg.trabalhofinal
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.UriMatcher
+import android.database.Cursor
+import android.net.Uri
+import android.provider.BaseColumns
+
+class ContentProviderTrabalhoFinal: ContentProvider() {
+    var dbOpenHelper : BDStandOpenHelper? = null
+
+
+    override fun onCreate(): Boolean {
+        dbOpenHelper = BDStandOpenHelper(context)
+
+        return true
+    }
+
+    override fun query(
+        p0: Uri,
+        p1: Array<out String>?,
+        p2: String?,
+        p3: Array<out String>?,
+        p4: String?
+    ): Cursor? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getType(p0: Uri): String? {
+        TODO("Not yet implemented")
+    }
+
+    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
+        TODO("Not yet implemented")
+    }
+}
