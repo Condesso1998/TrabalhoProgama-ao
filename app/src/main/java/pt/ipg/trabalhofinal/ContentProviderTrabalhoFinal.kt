b75068f8da2a5c04fcd1caa60c451c90b6ceb796
Condesso1998 <ruicondesso98@gmail.com>
2022-06-19 17:58:31 +0100

    implementaçao da query ContentProvider

--- app/src/main/java/pt/ipg/trabalhofinal/ContentProviderTrabalhoFinal.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/ContentProviderTrabalhoFinal.kt
@@ -24,11 +24,28 @@ class ContentProviderTrabalhoFinal: ContentProvider() {
         selectionArgs: Array<out String>?,
         sortOrder: String?
     ): Cursor? {
-        TODO("Not yet implemented")
+        val db = dbOpenHelper!!.readableDatabase
+
+        requireNotNull(projection)
+        val colunas = projection as Array<String>
+
+        val argsSeleccao = selectionArgs as Array<String>?
+
+
+        val id = uri.lastPathSegment
+
+        val cursor = when (getUriMatcher().match(uri)) {
+            URI_Carros -> TabelaBDCarros(db).query(colunas, selection, argsSeleccao, null, null, sortOrder)
+            URI_Clientes -> TabelaBDCliente(db).query(colunas, selection, argsSeleccao, null, null, sortOrder)
+            URI_Carros_ESPECIFICO -> TabelaBDCarros(db).query(colunas, "${BaseColumns._ID}=?", arrayOf("${id}"), null, null, null)
+            URI_Cliente_ESPECIFICO -> TabelaBDCliente(db).query(colunas, "${BaseColumns._ID}=?", arrayOf("${id}"), null, null, null)
+            else -> null
+        }
+
+        db.close()
+
+        return cursor
     }
-
-
-
 
 
 
