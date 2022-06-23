69c406f0a65769217a0b89724b7eacb182adbaf8
Condesso1998 <ruicondesso98@gmail.com>
2022-06-23 20:07:34 +0100

    Desenho Interface MenuPrincipal

--- app/src/main/java/pt/ipg/trabalhofinal/MenuFirstFragment.kt
+++ app/src/main/java/pt/ipg/trabalhofinal/MenuFirstFragment.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import pt.ipg.trabalhofinal.databinding.FragmentFirstBinding
+import pt.ipg.trabalhofinal.databinding.ActivityMainBinding.inflate
+import pt.ipg.trabalhofinal.databinding.FragmentMenuPrincipalBinding
+import pt.ipg.trabalhofinal.databinding.FragmentSecondBinding.inflate
 
 class MenuFirstFragment: Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentMenuPrincipalBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -21,7 +23,7 @@ class MenuFirstFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentMenuPrincipalBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -29,7 +31,7 @@ class MenuFirstFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.buttonCliente.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
